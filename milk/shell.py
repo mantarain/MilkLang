d4683cc3075d6bd62ef6906699de14bb45ed5c85
Mantarain <94607693+mantarain@users.noreply.github.com>
2023-03-08 08:00:05 +0000

    Commit #0005 - Created nodes.py files for parser

--- milk/shell.py
+++ milk/shell.py
@@ -1,15 +1,16 @@
-import src.main as main
+from src import main
 
 print("Milk 1")
-print("v0.1") # TODO: extract info from info.txt and display instead of here
+print("v0.1")  # TODO: extract info from info.txt and display instead of here
 
 while True:
     text = input("Milk > ")
-    if text.strip() == "": continue
+    if text.strip() == "":
+        continue
     result, error = main.run("<console>", text)
 
     if error:
         print(error.as_string())
-    
+
     elif result:
         print(result)
