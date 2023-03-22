db53683b570c9b8cb7049d1462c6ad84586763e0
Mantarain <94607693+mantarain@users.noreply.github.com>
2023-03-22 13:13:28 +0000

    condensed f

--- milk/src/context.py
+++
@@ -1,7 +0,0 @@
-
-class Context:
-  def __init__(self, displayName, parent=None, parentEntryPos=None):
-    self.displayName = displayName
-    self.parent = parent
-    self.parentEntryPos = parentEntryPos
-    self.symbolTable = None
