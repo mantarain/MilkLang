ea854e8bb10afa30b3a7a474c9e860bc0ffbd1ef
Mantarain <94607693+mantarain@users.noreply.github.com>
2023-03-25 07:57:19 +0000

    reset main.py, shell.py because of weird "corruption"

--- milk/src/shell.py
+++ milk/src/shell.py
@@ -1,12 +1,14 @@
 import main
 
 while True:
-    text = input('milk > ')
-    if text.strip() == "":
-        continue
-    result, error = main.run('<console>', text)
+	text = input('milk > ')
+	if text.strip() == "": continue
+	result, error = main.run('<terminal>', text)
 
-    if error:
-        print(error.as_string())
-    elif result:
-        print(result)
+	if error:
+		print(error.as_string())
+	elif result:
+		if len(result.elements) == 1:
+			print(repr(result.elements[0]))
+		else:
+			print(repr(result))
